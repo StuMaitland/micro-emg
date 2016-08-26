53a6f44db4c04432d4a1a3051f1b2a40f10d9333
Stuart Maitland <stuart@mediwikis.com>
2016-08-26 15:17:37 +0100

    Chang monstrous byte concatenation to lovely Python Unpack function

--- ok-interface/test.py
+++ ok-interface/test.py
@@ -3,6 +3,7 @@ import sys
 import string
 import time
 import numpy
+import struct
 
 
 class DESTester:
@@ -102,8 +103,8 @@ class DESTester:
         self.xem.UpdateWireIns()
         self.encryptDecrypt(infile, outfile)
 
-    def lightPulse(self,i=1):
-        #Cascades lights on OK machine
+    def lightPulse(self, i=1):
+        # Cascades lights on OK machine
         while 1:
             if i > 255:
                 i = 1
@@ -112,68 +113,57 @@ class DESTester:
             time.sleep(0.1)
             i *= 2
 
-    def startRecording(self,RecordingState):
-        #Switches the board to a listening state
+    def startRecording(self, RecordingState):
+        # Switches the board to a listening state
         if RecordingState:
             self.xem.ActivateTriggerIn(0x41, 0)
         else:
             self.xem.ActivateTriggerIn(0x41, 1)
 
     def isRunning(self):
-        #Checks whether the board is currently evaluating a data block
+        # Checks whether the board is currently evaluating a data block
         return self.xem.GetWireOutValue(0x22)
 
-    def setContinuousRunMode(self,continuousMode):
-        #Set the continuous recording mode i.e. run whether the computer is ready for data or not
+    def setContinuousRunMode(self, continuousMode):
+        # Set the continuous recording mode i.e. run whether the computer is ready for data or not
         if continuousMode:
-            self.xem.SetWireInValue(0x00,0x02,0x02)
+            self.xem.SetWireInValue(0x00, 0x02, 0x02)
         else:
-            self.xem.SetWireInValue(0x00,0x00,0x02)
+            self.xem.SetWireInValue(0x00, 0x00, 0x02)
         self.xem.UpdateWireIns()
 
-    def setMaxTimeStep(self,maxTimeStep):
-        #Set the amount of time the board should listen for data over
+    def setMaxTimeStep(self, maxTimeStep):
+        # Set the amount of time the board should listen for data over
 
         # I have no idea why you need to do this least/most sig bit but its in the source so
         maxTimeStepLsb = maxTimeStep & 0x0000ffff
         maxTimeStepMsb = maxTimeStep & 0xffff0000
 
         # These are just the magic addresses for switching on the timers
-        self.xem.SetWireInValue(0x01,maxTimeStepLsb)
-        self.xem.SetWireInValue(0x02,maxTimeStepMsb >> 16)
+        self.xem.SetWireInValue(0x01, maxTimeStepLsb)
+        self.xem.SetWireInValue(0x02, maxTimeStepMsb >> 16)
         self.xem.UpdateWireIns()
 
-
     def collectDataFromPipeOut(self, length, sampleLength):
-        #Starts the timer, waits until an amount of data is available on the USB buffer, then collects it
+        # Starts the timer, waits until an amount of data is available on the USB buffer, then collects it
         self.setMaxTimeStep(sampleLength)
         buffer = bytearray("\x00" * length)
         self.startRecording(1)
         while self.isRunning():
             pass
         # Read from the Data pipe out- note that buffer passed is overwritten with the new data source in doing this.
-        self.xem.ReadFromPipeOut(0xa0,buffer)
+        self.xem.ReadFromPipeOut(0xa0, buffer)
         return buffer
 
-    def convertUSBWord(self,integers):
-        #Takes bytes and concatenates them into a new word
-        multiple=len(integers)
-        concatItems=0
-        #Reverse the array for little endian format
-        for item in integers[::-1]:
-            concatItems=concatItems + (item << (multiple * 8))
-            multiple-= 1
+    def enableDataStream(self, streamIndex, enable):
+        # Enables or disables streams
+        return None
 
-        #Trim the final byte caused by initialising with 0
-        concatItems=bin(concatItems)[:-8]
-        concatItems=int(str(concatItems),2)
+    def readDataBlock(self, buffer, samplesPerDataBlock, numDataStreams):
 
-        return concatItems
+        # Takes raw data from the USB buffer on the board and converts it into something usable.
 
-
-    def readDataBlock(buffer, samplesPerDataBlock, numDataStreams):
-
-        #First we initialise some variables
+        # First we initialise some local variables
         timeStamp = []
         auxiliaryData = numpy.zeros((1, 1, 1,))
         amplifierData = numpy.zeros((1, 1, 1,))
@@ -183,56 +173,65 @@ class DESTester:
         index = 0
 
         # The data is arranged in the following format on the board:
-            ## 8 byte header
-            ## 4 byte timestamp
-            ## n byte auxiliary data
-            ## n byte amplifier data (the one we want)
-            ## n byte filler word
-            ## 128 byte ADC5662 data
-            ## 4 byte ttlIn + ttOut data
+        ## 8 byte header
+        ## 4 byte timestamp
+        ## n byte auxiliary data
+        ## n byte amplifier data (the one we want)
+        ## n byte filler word
+        ## 128 byte ADC5662 data
+        ## 4 byte ttlIn + ttOut data
 
         for sample in range(0, samplesPerDataBlock):
+
+            # OK, there's a lot going on on this line.
+            # Unpack the byte array, coerced into a bytes object
+            # < means little endian, Q indicates unsigned long long
+            # By default returns a tuple, so just get the first bit.
+            # Reference: https://docs.python.org/3/library/struct.html
+            header=struct.unpack("<Q",bytes(buffer[index:index+8]))[0]
             # Check the first 8 bytes to match the RHD2000 magic header word
-            if des.convertUSBWord(buffer[index:index + 8]) != 0xc691199927021942:
+            if header != 0xc691199927021942:
                 raise SyntaxError("Error in readDataBlock: Incorrect header")
             index += 8
 
             # Read the timestamp
-            timeStamp.append(des.convertUSBWord(buffer[index:index + 4]))
+            #This time it's L- unsigned long
+
+            timeStamp.append(struct.unpack("<L",buffer[index:index+4])[0])
             index += 4
 
             # Read auxiliary input
             for channel in range(0, 3):
                 for stream in range(0, numDataStreams):
-                    auxiliaryData[stream][channel][sample] == des.convertUSBWord(buffer[index:index + 1])
+                    # The rest are H, unsigned short
+                    auxiliaryData[stream][channel][sample] = struct.unpack("<H",buffer[index:index+2])[0]
                     index += 2
                     # Each sample is represented by two bytes
 
             # Read amplifier channels- this is the one we really want
             for channel in range(0, 32):
                 for stream in range(0, numDataStreams):
-                    amplifierData[stream][channel][sample] = des.convertUSBWord(buffer[index:index + 1])
-                    index+=2
+                    amplifierData[stream][channel][sample] = struct.unpack("<H",buffer[index:index+2])[0]
+                    index += 2
             # Skip 36th filler word in each data stream
             index += 2 * numDataStreams
 
             # Read from AD5662 ADCs
             for i in range(0, 8):
-                boardAdcData[i][t] = des.convertUSBWord(buffer[index:index + 1])
+
+                boardAdcData[i][sample] = struct.unpack("<H",buffer[index:index+2])[0]
                 index += 2
 
             # Read TTL input and output values
-            ttlIn[sample] = des.convertUSBWord(buffer[index:index + 1])
+            ttlIn[sample] = struct.unpack("<H",buffer[index:index+2])[0]
             index += 2
-            ttlOut[sample] = des.convertUSBWord(buffer[index:index + 1])
+            ttlOut[sample] = struct.unpack("<H",buffer[index:index+2])[0]
             index += 2
 
 
-
-
-#User-modified variables
-samplesPerDataBlock=60
-numDataStreams= 5
+# User-modified variables
+samplesPerDataBlock = 60
+numDataStreams = 5
 
 # Main code
 print("------ DES Encrypt/Decrypt Tester in Python ------")
@@ -242,14 +241,12 @@ completed = des.initializeDevice()
 if completed == 'False':
     exit
 
-#des.lightPulse()
-
+# des.lightPulse()
 
 des.setContinuousRunMode(False)
 
-buffer=des.collectDataFromPipeOut(2048, samplesPerDataBlock)
-des.readDataBlock(buffer,samplesPerDataBlock,numDataStreams)
-
+buffer = des.collectDataFromPipeOut(2048, samplesPerDataBlock)
+des.readDataBlock(buffer, samplesPerDataBlock, numDataStreams)
 
 print("ready for input")
 print("")
